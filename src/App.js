817446221b42e63f01886fe13442d5c37f7a5c8b
kfrn <knfrances@gmail.com>
2017-04-12 15:20:45 +1200

    Redux working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = ({state, dispatch}) => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to React</h2>
       </div>
-    )
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>The initial state is {state.text}</p>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+import App from './App'
+import reducer from './redux/reducer'
+
+const root = document.getElementById('root')
+const store = createStore(reducer, devToolsEnhancer())
+
+store.subscribe( () => {
+  render(
+    <Provider store={store}>
+      <App state={store.getState()} dispatch={store.dispatch}/>
+    </Provider>,
+    root
+  )
+})
+
+store.dispatch({type: 'INIT'})
