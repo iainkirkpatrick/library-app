18299ff160d18890f76cbbdb9fa90a0ca8557282
kfrn <knfrances@gmail.com>
2017-04-14 15:00:17 +1200

    Add active class to nav links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
+    <nav>
+      <div className="collapse navbar-collapse">
         <div className="navbar-header">
-          <a className="navbar-brand" href="#">Murakami books</a>
+          <a className="navbar-brand" href="/">Murakami books</a>
         </div>
-        <div className="collapse navbar-collapse" id="bookshop-navbar-collapse">
-          <ul className="nav navbar-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/books">Books</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-          </ul>
-        </div>
+        <ul className="nav navbar-nav">
+          <li><NavLink activeClassName="active" to="/books">Books</NavLink></li>
+          <li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+          <li><NavLink activeClassName="active" to="/cart">Cart</NavLink></li>
+        </ul>
       </div>
     </nav>
   )
 }
 
 export default NavBar
-
-// const NavBar = () => {
-//   return (
-//     <nav className="navbar navbar-default">
-//       <div className="container-fluid">
-//         <div className="navbar-header">
-//           <a className="navbar-brand" href="#">Murakami books</a>
-//         </div>
-//         <div className="collapse navbar-collapse" id="bookshop-navbar-collapse">
-//           <ul className="nav navbar-nav">
-//             <li><a href="/">Home</a></li>
-//             <li><a href="/about">About</a></li>
-//             <li><a href="/books">Books</a></li>
-//             <li><a href="/cart">Cart</a></li>
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
